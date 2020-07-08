dd5416d15b0bb7878935827aba316d3b9a36d553
Ryan Hale <ryan@ryanjhale.com>
2020-07-08 17:52:51 +0200

    correct adding of Google and Facebook pixels in headers

--- header-landing.php
+++ header-landing.php
@@ -2,51 +2,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <?php
-	if(CFM_URL == 'https://comefollowme.it') {
-		?>
-		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165691891-4"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-		
-		  gtag('config', 'UA-165691891-4');
-		</script>
-	
-		<?php
-	} elseif(CFM_URL == 'https://vienieseguimi.it') {
-		?>
-		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165691891-5"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-		
-		  gtag('config', 'UA-165691891-5');
-		</script>
-
-		<?php
-	} elseif(CFM_URL == 'https://viensetsuismoi.it') {
-		?>
-		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165691891-6"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-		
-		  gtag('config', 'UA-165691891-6');
-		</script>
-		
-		<?php
-	}
-	?>
+    <?php include 'pixel-tracking.php'; ?>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
     <?php if ( ! ( function_exists( 'has_site_icon' ) && has_site_icon() ) ): ?>
