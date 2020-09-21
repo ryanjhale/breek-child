93b364636a56db9a2fb8e64921f5c82ab39cf6f6
Ryan Hale <ryan@ryanjhale.com>
2020-09-21 18:51:43 +0200

    only fb pixel tracking active

--- pixel-tracking.php
+++ pixel-tracking.php
@@ -1,5 +1,6 @@
 <?php
 if(isset($_REQUEST['source'])) {
+	
 	if($_REQUEST['source'] == 'comefollowme') {
 		?>
 		<!-- Facebook Pixel Code -->
@@ -21,6 +22,7 @@ if(isset($_REQUEST['source'])) {
 		<!-- End Facebook Pixel Code -->
 		<?php
 	}
+	
 	if($_REQUEST['source'] == 'vienieseguimi') {
 		?>
 		<!-- Facebook Pixel Code -->
@@ -42,6 +44,7 @@ if(isset($_REQUEST['source'])) {
 		<!-- End Facebook Pixel Code -->
 		<?php
 	}
+	
 	if($_REQUEST['source'] == 'viensetsuismoi') {
 		?>
 		<!-- Facebook Pixel Code -->
@@ -63,8 +66,75 @@ if(isset($_REQUEST['source'])) {
 		<!-- End Facebook Pixel Code -->
 		<?php
 	}
-}
+	
+} else {
+	
+	if(CFM_URL == 'https://comefollowme.it') {
+		?>
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '1369917960065599');
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=1369917960065599&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+		<?php
+	} 
 
+	if(CFM_URL == 'https://vienieseguimi.it') {
+		?>
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '271742514245706');
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=271742514245706&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+		<?php
+	}
+
+	if(CFM_URL == 'https://viensetsuismoi.it') {
+		?>
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '721935858565898');
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=721935858565898&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+		<?php
+	}
+/*
 if(CFM_URL == 'https://comefollowme.it') {
 	?>
 	
@@ -111,4 +181,5 @@ if(CFM_URL == 'https://comefollowme.it') {
 	
 	<?php
 }
+*/
 ?>
