d3c10faa40da1f26321d3674dc42a474c65bbb98
Ryan Hale <ryan@ryanjhale.com>
2020-12-10 10:31:13 +0100

    scroll middle

--- lesson-modal-bootstrap.php
+++ lesson-modal-bootstrap.php
@@ -1,11 +1,11 @@
-<div class="modal fade " id="lesson-modal" tabindex="-1" role="dialog" aria-labelledby="important-msg-label" aria-hidden="true">
+<div class="modal fade " id="lesson-modal" tabindex="-1" role="dialog" aria-labelledby="important-msg-label" aria-hidden="true" style="display: block !important;">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content" style="overflow-y: initial !important;">
           	<div class="modal-header">
             	<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 				<h4 class="modal-title" id="important-msg-label"></h4>
           	</div>
-		  	<div class="modal-body">
+		  	<div class="modal-body" style="height: 80vh; overflow-y: auto;">
           	</div>
 		  	<div class="modal-footer">
             	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
