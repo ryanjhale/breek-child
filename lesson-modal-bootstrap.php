0bba08c2ac37aaefe958246043b2b60f9627b73b
Ryan Hale <ryan@ryanjhale.com>
2021-02-04 10:25:36 +0100

    move cookie functions

--- lesson-modal-bootstrap.php
+++ lesson-modal-bootstrap.php
@@ -49,6 +49,27 @@ ul {
 
 <script>
 	
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+	
 function getModalContent(post_id) {
 	
 	// jQuery('.modal-body').empty();
@@ -189,26 +210,7 @@ jQuery(document).ready(function() {
 	
 	
 	
-	function readCookie(name) {
-	    var nameEQ = name + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0;i < ca.length;i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-	        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	    }
-	    return null;
-	}
 	
-	function createCookie(name,value,days) {
-	    if (days) {
-	        var date = new Date();
-	        date.setTime(date.getTime()+(days*24*60*60*1000));
-	        var expires = "; expires="+date.toGMTString();
-	    }
-	    else var expires = "";
-	    document.cookie = name+"="+value+expires+"; path=/";
-	}
 	
 	
 	jQuery(document).on('click','.respondsubmit',function(e) {
