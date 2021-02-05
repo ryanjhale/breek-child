9ff02a54e74cc64ffcfe56c10dc9721666a7aab0
Ryan Hale <ryan@ryanjhale.com>
2021-02-05 16:57:05 +0100

    console log

--- lesson-modal-bootstrap.php
+++ lesson-modal-bootstrap.php
@@ -52,6 +52,7 @@ ul {
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
+    console.log(ca);
     for(var i=0;i < ca.length;i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
