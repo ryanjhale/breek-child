56d49e7c3cbbaa4089a70cb156216152493f4226
Ryan Hale <ryan@ryanjhale.com>
2021-02-07 18:15:07 +0100

    styling

--- functions/ajax.php
+++ functions/ajax.php
@@ -204,7 +204,7 @@ function cfm_handle_response(){
 	// setcookie('name', sanitize_text_field($_REQUEST['respondent']), $expiration);
 	// setcookie('email', sanitize_text_field($_REQUEST['email']), $expiration);
 
-	echo json_encode(array('message' => '<div class="alert alert-success" role="alert">Thank you!  We received your response and our team will follow up with you soon.</div>'));
+	echo json_encode(array('message' => 'Success!'));
 	
 	wp_die();
 }
