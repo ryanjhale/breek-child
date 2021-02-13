a12eadfcbbb5fda11ffc8d07efa4cde2ed1ad83e
Ryan Hale <ryan@ryanjhale.com>
2021-02-13 10:25:07 +0100

    load child post formats

--- functions.php
+++ functions.php
@@ -65,7 +65,7 @@ class epcl_theme_setup {
 		/* Main Includes */
 
 		require_once(EPCL_ABSPATH.'/functions/import/import-demo.php');
-		require_once(EPCL_ABSPATH.'/functions/post-formats.php');
+		require_once(get_theme_file_path('functions/post-formats.php'));
 		require_once(get_theme_file_path('functions/ajax.php'));
         require_once(EPCL_ABSPATH.'/functions/enqueue-scripts.php');
         require_once(EPCL_ABSPATH.'/functions/color-helper.php');
