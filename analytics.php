0e61d688e56d1d5484dd33c6bbf4bd2499c43053
ryanjhale <ryan@ryanjhale.com>
2023-03-09 19:05:54 +0100

    analytics

--- analytics.php
+++ analytics.php
@@ -3,37 +3,29 @@ if(!is_user_logged_in()) {
 		
 	if(CFM_ENV == 'prod-english') {
 		?>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165691891-4"></script>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-NNHKNN51WG"></script>
 		<script>
 		  window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 		
-		  gtag('config', 'UA-165691891-4');
+		  gtag('config', 'G-NNHKNN51WG');
 		</script>
-		<!-- End Google Analytics -->
 		<?php
 	}
 	
 	if(CFM_ENV == 'prod-italian') {
 		?>			
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165691891-5"></script>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-53QV1T22D4"></script>
 		<script>
 		  window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 		
-		  gtag('config', 'UA-165691891-5');
+		  gtag('config', 'G-53QV1T22D4');
 		</script>
-		<!-- End Google Analytics -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-5LR3R3B');</script>
-		<!-- End Google Tag Manager -->
 
 		<?php
 	}
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<?php // include 'analytics.php'; ?>
+	<?php include 'analytics.php'; ?>
 	<?php // include 'mailchimp.php'; ?>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
